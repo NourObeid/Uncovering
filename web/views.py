41fb11c265824053c760095ef14e64e3b4b3331a
dennis9707 <he0707@hotmail.com>
2018-02-07 17:45:29 +0000

    basic outline

--- web/views.py
+++ web/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
 # Create your views here.
 def post_list(request):
     return render(request, 'web/home.html', {})
 
 def index(request):
-    return HttpResponse("Home")
+    template = loader.get_template('web/home.html')
+    return HttpResponse('')
