ddb0e36abc4966534d9a36dca5f5ecf734f146e6
dennis9707 <he0707@hotmail.com>
2018-03-06 01:30:55 +0000

    edited results

--- web/forms.py
+++ web/forms.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
-
 class UploadFileForm(forms.Form):
     file = forms.FileField()
 
--- web/wikidata.py
+++
@@ -1,8 +0,0 @@
-
-import pywikibot
-
-site = pywikibot.Site("en", "wikipedia")
-page = pywikibot.Page(site, "Douglas Adams")
-item = pywikibot.ItemPage.fromPage(page)
-
-print(item)
